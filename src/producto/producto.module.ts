475604ab129e9ea1bf1ff76f2376a5965a2c5512
Gonza149 <elgonza149@gmail.com>
2025-10-16 03:36:16 -0300

    refactor(producto): implementa validador de existencia

--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ProductoService } from './producto.service';
 import { ProductoController } from './producto.controller';
+import { CategoriaRepository } from 'src/categoria/categoria.repository';
+import { MarcaModule } from 'src/marca/marca.module';
+import { CategoriaModule } from 'src/categoria/categoria.module';
+import { PrismaProductoRepository } from './repositories/prisma-producto.repository';
+import { IProductoRepositoryToken } from './repositories/producto.repository.interface';
+import { ProductoValidator } from './producto.validator';
+
 
 @Module({
+  imports: [MarcaModule, CategoriaModule],
   controllers: [ProductoController],
-  providers: [ProductoService],
+  providers: [
+    ProductoService,
+    CategoriaRepository,
+    {
+      provide: 'IProductoRepository',
+      useClass: PrismaProductoRepository,
+
+    },
+    ProductoValidator,
+
+  ],
+  exports: [ProductoService, IProductoRepositoryToken],
 })
 export class ProductoModule {}
