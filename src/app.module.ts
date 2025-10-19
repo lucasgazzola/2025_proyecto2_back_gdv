14bea137181bae5460b958bf90ff3c01a90386fe
Gonza149 <elgonza149@gmail.com>
2025-10-19 08:25:57 +0000

    chore(app): agrega modulo de usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { MarcaModule } from './marca/marca.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { ProductoModule } from './producto/producto.module';
 import { FacturaModule } from './factura/factura.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [MarcaModule, CategoriaModule, ProductoModule, FacturaModule],
+  imports: [MarcaModule, CategoriaModule, ProductoModule, FacturaModule, UsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
