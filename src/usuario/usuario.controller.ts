945899bce90d0472c8b8063beb279f8c58feaad2
Gonza149 <elgonza149@gmail.com>
2025-10-19 00:32:41 +0000

    refactor(usuario): implementa repositoy y mapper

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsuarioService } from './usuario.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
 
 @Controller('usuarios')
 export class UsuarioController {
@@ -17,18 +18,23 @@ export class UsuarioController {
     return this.usuarioService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usuarioService.findOne(+id);
+  @Get('email/:email')
+  findByEmail(@Param('email') email: string) {
+    return this.usuarioService.findByEmail(email);
   }
 
+  @Get(':id')
+  findById(@Param('id', ParseIntPipe) id: number) {
+    return this.usuarioService.findById(id);
+  }
+  
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
-    return this.usuarioService.update(+id, updateUsuarioDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateUsuarioDto: UpdateUsuarioDto) {
+    return this.usuarioService.update(id, updateUsuarioDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usuarioService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.usuarioService.remove(id);
   }
 }
--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
+import { PrismaUsuarioRepository } from './repositories/prisma-usuario.repository';
+import { IUsuarioRepositoryToken } from './repositories/usuario.repository.interface';
 
 @Module({
   controllers: [UsuarioController],
-  providers: [UsuarioService],
-  exports: [UsuarioService],
+  providers: [
+    UsuarioService,
+    {
+      provide: IUsuarioRepositoryToken,
+      useClass: PrismaUsuarioRepository,
+    },
+  ],
+  exports: [UsuarioService, IUsuarioRepositoryToken],
 })
 export class UsuarioModule {}
