b1ac94840a94c84b95c72a83f6633e74c8e9b2ce
Gonza149 <elgonza149@gmail.com>
2025-10-20 01:47:03 -0300

    chore(app): actualiza imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,12 @@ import { CategoriaModule } from './categoria/categoria.module';
 import { ProductoModule } from './producto/producto.module';
 import { FacturaModule } from './factura/factura.module';
 import { UsuarioModule } from './usuario/usuario.module';
+import { LogsModule } from './logs/logs.module';
+import { AuthModule } from './auth/auth.module';
+import { ProveedorModule } from './proveedor/proveedor.module';
 
 @Module({
-  imports: [MarcaModule, CategoriaModule, ProductoModule, FacturaModule, UsuarioModule],
+  imports: [MarcaModule, CategoriaModule, ProductoModule, FacturaModule, UsuarioModule, LogsModule, AuthModule, ProveedorModule],
   controllers: [AppController],
   providers: [AppService],
 })
