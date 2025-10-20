e0054565645a1d6d1ee5eb7a215d56ef70b01e02
Gonza149 <elgonza149@gmail.com>
2025-10-20 01:43:57 -0300

    feat: Implementa parcialmente logs

---
+++ src/logs/logs.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { LogsService } from './logs.service';
+import { LogsController } from './logs.controller';
+import { LogRepository } from './repositories/log.repository';
+import { ILogRepositoryToken } from './repositories/log.repository.interface';
+
+@Module({
+  controllers: [LogsController],
+  providers: [
+    LogsService,
+    {
+      provide: ILogRepositoryToken,
+      useClass: LogRepository,
+    },
+  ],
+})
+export class LogsModule {}
