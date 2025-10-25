8cc39bb16f16df83e18077e6bc83d80316f45826
Lucas Gazzola <lucasgazzola1@outlook.com>
2025-10-25 16:11:18 -0300

    Fix

--- src/cliente/cliente.module.ts
+++ src/cliente/cliente.module.ts
@@ -3,9 +3,11 @@ import { ClienteController } from './cliente.controller';
 import { ClienteService } from './cliente.service';
 import { IClienteRepositoryToken } from './repositories/cliente.repository.interface';
 import { PrismaClienteRepository } from './repositories/prisma-cliente.repository';
+import { LogsModule } from 'src/logs/logs.module';
 
 @Module({
   controllers: [ClienteController],
+  imports: [LogsModule],
   providers: [
     ClienteService,
     {
